84e40eedaadae47441201c82ecf42d0314c07843
Mateo Vicentin <mateov@bluetrailsoft.com>
2023-04-18 10:43:52 -0300

    Revert "Modified functionality... couldn't do without doing it I guess?. Will look forward to KATA video to modify my code if it does not respect O/C principle"
    This reverts commit 5d99213123587a59ae0632b5f8a644146f484b8e.

--- customer-csv-file-writer.ts
+++ customer-csv-file-writer.ts
@@ -6,24 +6,6 @@ export class CustomerCsvFileWriter {
   public writeCustomers(fileName: string, customers: Customer[]) {
     if (customers === null) throw new Error('Null customers');
 
-    const originalCustomers = [...customers];
-
-    if (customers.length > 10) {
-      for (let i = 0; i <= customers.length; i += 10) {
-        const fileNameInIteration = fileName.split('.');
-        fileNameInIteration[0] = `${fileNameInIteration[0]}-${i / 10}`;
-
-        this.writeTenCustomers(
-          originalCustomers.splice(0, 10),
-          fileNameInIteration.join('.'),
-        );
-      }
-    } else {
-      this.writeTenCustomers(originalCustomers, fileName);
-    }
-  }
-
-  private writeTenCustomers(customers: Customer[], fileName: string) {
     const copiedCustomers: Customer[] = [...customers];
 
     for (const customer of copiedCustomers) {
